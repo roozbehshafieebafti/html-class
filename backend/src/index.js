66c4fa5669d7c2dacf4699136f7d67e2c8779065
roozbeh <45459584+roozbehshafieebafti@users.noreply.github.com>
2021-11-14 13:39:49 +0330

    update backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const faker = require('faker');
+
+let users=[];
+setInterval(() => {
+    users=[];
+}, (60*60*24)*1000);
 
 const app = express();
+app.use(cors({origin:'*'}))
 app.use(express.json()) 
-const port = 80;
+const port = 3000;
+
+app.get('/',(req, res)=>{
+    res.status(200).json({answer: 'hello world!'});        
+});
 
 
+// MATH //
 app.get('/plus', (req, res) => {   
     const {a ,b } =  req.query;
     if(a && b){
@@ -80,6 +92,8 @@ app.post('/div', (req, res) => {
 });
 
 
+
+// FORM //
 app.post('/form', (req, res) => {
     const {first_name, last_name} = req.body;
     if(first_name && last_name){
@@ -88,7 +102,128 @@ app.post('/form', (req, res) => {
     else{
         res.status(400).json({message: 'your data is in correct'});
     }
-})
+});
+
+
+app.get('/users',(req,res)=>{
+    if(users.length===0){
+        for (let index = 0; index < 10; index++) {
+            let firstName = faker.name.firstName();
+            let lastName = faker.name.lastName();    
+            let jobTitle = faker.name.jobTitle();
+            let prefix = faker.name.prefix(); 
+            let suffix = faker.name.suffix();
+            let jobArea = faker.name.jobArea();    
+            let phone = faker.phone.phoneNumber();
+            let image = faker.image.image();
+            
+            users.push({
+                id: index,
+                firstName,
+                lastName,
+                jobTitle,
+                prefix,
+                suffix,
+                jobArea,
+                phone,
+                image
+            });
+        }  
+    }
+    res.status(200).json(users);
+});
+
+app.post('/users/create',(req,res)=>{ 
+    const { 
+        firstName,
+        lastName,
+        jobTitle,
+        prefix,
+        suffix,
+        jobArea,
+        phone,
+        image 
+    } = req.body;
+
+ 
+    const data = {};
+    if(firstName){
+        data['firstName'] = firstName;
+    }
+    if(lastName){
+        data['lastName'] = lastName;            
+    }
+    if(jobTitle){
+        data['jobTitle'] = jobTitle;            
+    }
+    if(prefix){
+        data['prefix'] = prefix;            
+    }
+    if(suffix){
+        data['suffix'] = suffix;            
+    }
+    if(jobArea){
+        data['jobArea'] = jobArea;            
+    }
+    if(phone){
+        data['phone'] = phone;
+    }
+    if(image){
+        data['image'] = image;
+    }
+    users.push(data);      
+    res.status(201).json({message: 'اطلاعات با موفقیت ذخیره شد'});
+
+  
+});
+
+app.put('/users/update',(req,res)=>{ 
+    const { id,
+        firstName,
+        lastName,
+        jobTitle,
+        prefix,
+        suffix,
+        jobArea,
+        phone,
+        image 
+    } = req.body;
+
+    if(id!==null || id!==undefined){
+        const data = users[id];
+        if(firstName){
+            data['firstName'] = firstName;
+        }
+        if(lastName){
+            data['lastName'] = lastName;            
+        }
+        if(jobTitle){
+            data['jobTitle'] = jobTitle;            
+        }
+        if(prefix){
+            data['prefix'] = prefix;            
+        }
+        if(suffix){
+            data['suffix'] = suffix;            
+        }
+        if(jobArea){
+            data['jobArea'] = jobArea;            
+        }
+        if(phone){
+            data['phone'] = phone;
+        }
+        if(image){
+            data['image'] = image;
+        }
+        users[id] = users[id];
+        
+        res.status(200).json({message: 'اطلاعات با موفقیت آپدیت شد'});
+    }
+    else{
+        res.status(400).json({message: 'آیدی ارسال شده اشتباه است'});
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`app listen on http://localhost:${port}`)
